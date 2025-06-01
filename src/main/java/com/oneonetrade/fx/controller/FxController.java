fa6be798c4ee3cf9f2b3f6d41bd12f09c13e3a87
lvgarfett <l714.100@gmail.com>
2025-06-01 12:49:05 +0800

    /fx/p/cnd

--- src/main/java/com/oneonetrade/fx/controller/FxController.java
+++ src/main/java/com/oneonetrade/fx/controller/FxController.java
@@ -10,15 +10,11 @@ import com.oneonetrade.fx.service.FxService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -31,10 +27,52 @@ public class FxController {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
 
+    @PostMapping("/p/cnd")
+    public ResponseEntity<?> searchFxRateByBody(@RequestBody Cnd apiRequest) throws JsonProcessingException {
 
+        LocalDate oneYearAgo = LocalDate.now().minusYears(1);
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+
+        try {
+
+            LocalDate startDate = LocalDate.parse(
+                    apiRequest.getStartDate().replace("/",""),
+                    FORMATTER
+            );
+
+            LocalDate endDate = LocalDate.parse(
+                    apiRequest.getEndDate().replace("/",""),
+                    FORMATTER
+            );
+
+            if (startDate.isBefore(oneYearAgo) || endDate.isAfter(yesterday)) {
+                ApiError error = new ApiError("E001", "日期區間不符");
+                return ResponseEntity.ok(new ApiResponseFailed(error));
+            }
+
+        }
+
+        catch (DateTimeParseException e) {
+
+            ApiError error = new ApiError("E002", "日期格式不符");
+            return ResponseEntity.ok(new ApiResponseFailed(error));
+        }
+
+        List<?> result = fxService.searchFxRate(apiRequest);
+
+        if (result.size() > 0) {
+            ApiError error = new ApiError("0000", "成功");
+            return ResponseEntity.ok(new ApiResponseSuccess<>(error, result));
+        }
+
+        else {
+            ApiError error = new ApiError("E000", "查無資料");
+            return ResponseEntity.ok(new ApiResponseFailed(error));
+        }
+    }
 
     @GetMapping("/g/cnd")
-    public ResponseEntity<?> searchFxRate(@ModelAttribute Cnd apiRequest) throws JsonProcessingException {
+    public ResponseEntity<?> searchFxRateByParam(@ModelAttribute Cnd apiRequest) throws JsonProcessingException {
 
         LocalDate oneYearAgo = LocalDate.now().minusYears(1);
         LocalDate yesterday = LocalDate.now().minusDays(1);
